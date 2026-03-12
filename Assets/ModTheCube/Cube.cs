082d04030daf06fcd6e081a89a185ff8f0e20bca
caveiramica14 <micaeltiago2006@gmail.com>
2026-03-12 00:58:37 +0000

    passo 1, 2, 3 e 4
    Como os passos 1, 2, e 3 não é necessário fazer nada, envio todos os passos juntos

---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+
+    Material material;
+    float alpha;
+
+    void Start()
+    {
+        transform.position = new Vector3(5, 3, -6);
+        transform.localScale = Vector3.one * 2.4f;
+        
+        material = Renderer.material;
+
+        Color[] colors = {
+            new Color(0f, 0.3f, 0.5f, 1f),
+            new Color(0.5f, 0f, 0.1f, 1f),
+            new Color(0.2f, 0f, 0.5f, 1f)
+        };
+
+        material.color = colors[Random.Range(0, colors.Length)];
+    }
+
+    void Update()
+    {
+        transform.Rotate(10.0f * Time.deltaTime, 20.0f * Time.deltaTime, 0.0f);
+
+        alpha = Mathf.PingPong(Time.time, 1);
+
+        Color c = material.color;
+        c.a = alpha;
+        material.color = c;
+    }
+}
